3a00fdc761fb607865093b54c3806c138e4f3bbd
Axel Hempelt <a.hempelt@fiz-soft.de>
2025-04-21 15:23:51 +0200

    [TASK] added switch for choose color mode

---
+++ Resources/Public/JavaScripts/Menus/TopBar/ColorMode/Default.js
@@ -0,0 +1,100 @@
+(() => {
+  'use strict'
+
+  const getStoredTheme = () => localStorage.getItem('theme')
+  const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+  const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme) {
+      return storedTheme
+    }
+
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  }
+
+  const updateAriaPressedAndDescribedBy = (theme) => {
+    document.querySelectorAll('[data-bs-theme-value]').forEach(btn => {
+      const value = btn.getAttribute('data-bs-theme-value')
+      const isActive = document.documentElement.getAttribute('data-bs-theme') === value
+
+      btn.setAttribute('aria-pressed', isActive ? 'true' : 'false')
+
+      if (isActive) {
+        btn.setAttribute('aria-describedby', 'activeTheme')
+      } else {
+        btn.removeAttribute('aria-describedby')
+      }
+    })
+
+    // Optional: Inhalt des versteckten Status-Textes anpassen
+    const activeThemeStatus = document.getElementById('activeTheme')
+    if (activeThemeStatus) {
+      const readable = theme === 'light'
+        ? activeThemeStatus?.getAttribute('data-text-light') || 'Aktueller Modus: Hell'
+        : activeThemeStatus?.getAttribute('data-text-dark') || 'Aktueller Modus: Dunkel'
+      activeThemeStatus.textContent = readable
+    }
+  }
+
+  const setTheme = theme => {
+    if (theme === 'auto') {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+      document.documentElement.setAttribute('data-bs-theme', systemTheme)
+      updateAriaPressedAndDescribedBy(systemTheme)
+    } else {
+      document.documentElement.setAttribute('data-bs-theme', theme)
+      updateAriaPressedAndDescribedBy(theme)
+    }
+  }
+
+  const showActiveTheme = (theme, focus = false) => {
+    const themeSwitcher = document.querySelector('#bd-theme')
+    if (!themeSwitcher) {
+      return
+    }
+
+    const themeSwitcherText = document.querySelector('#bd-theme-text')
+    const activeThemeIcon = document.querySelector('.theme-icon-active use')
+    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+
+    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+      element.classList.remove('active')
+    })
+
+    btnToActive.classList.add('active')
+    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
+    themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+
+    if (focus) {
+      themeSwitcher.focus()
+    }
+  }
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme !== 'light' && storedTheme !== 'dark') {
+      const preferred = getPreferredTheme()
+      setTheme(preferred)
+      showActiveTheme(preferred)
+    }
+  })
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const preferred = getPreferredTheme()
+    setTheme(preferred)
+    showActiveTheme(preferred)
+
+    document.querySelectorAll('[data-bs-theme-value]')
+      .forEach(toggle => {
+        toggle.addEventListener('click', () => {
+          const theme = toggle.getAttribute('data-bs-theme-value')
+          setStoredTheme(theme)
+          setTheme(theme)
+          showActiveTheme(theme, true)
+        })
+      })
+  })
+})()

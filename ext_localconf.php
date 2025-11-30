86bde0a81e708128fcaba64953d6bb5de6032070
Oliver Thiele <mail@oliver-thiele.de>
2025-11-30 11:32:36 +0100

    [FEATURE] Register sk namespace for sitekit ViewHelpers

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,8 @@ use OliverThiele\OtSitekitbase\Components\ComponentCollection;
 
 defined('TYPO3') or die();
 
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['sk'][] = 'OliverThiele\OtSitekitbase\ViewHelpers';
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['skc'] = [
     ComponentCollection::class,
 ];
